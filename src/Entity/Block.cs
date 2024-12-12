c6e1429755f428f9d46f85d136442494c923426b
eryk <erykbabayan@gmail.com>
2024-12-12 08:28:03 +0100

    Add hashing functions to Block class

--- src/Entity/Block.cs
+++ src/Entity/Block.cs
@@ -1,26 +1,53 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Blockchain.Entity;
 
 public class Block
 {
     public int Index { get; set; }
     public DateTime CreatedAt { get; set; }
-    public required string PreviousHash { get; set; }
-    public required string Hash { get; set; }
-    public required List<Transaction> Transactions { get; set; }
+    public string PreviousHash { get; set; }
+    public string Hash { get; set; }
+    public List<Transaction> Transactions { get; set; }
     public long Nonce { get; set; }
 
-    public Block() //TODO
+    public Block(
+        DateTime createdAt,
+        List<Transaction> transactions,
+        string previousHash = ""
+    )
     {
-        //TODO
+        CreatedAt = createdAt;
+        Transactions = transactions;
+        PreviousHash = previousHash;
+        Hash = CalculateHash();
     }
 
     public string CalculateHash()
     {
-        return ""; //TODO
+        var sha256 = SHA256.Create();
+
+        var rawData = Index + CreatedAt.ToString("O") + PreviousHash + Nonce +
+                      string.Join(",", Transactions.Select(t => t.ToString()));
+
+        var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+        Console.WriteLine(bytes);
+        Console.WriteLine(BytesToHex(bytes));
+        
+        return BytesToHex(bytes);
     }
     
     private string BytesToHex(byte[] bytes)
     {
-        return ""; //TODO
+        var builder = new StringBuilder(bytes.Length * 2);
+
+        foreach (var b in bytes)
+        {
+            builder.Append(b.ToString("x2"));
+        }
+
+        return builder.ToString();
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Blockchain.Entity;
+
 Console.WriteLine("Hello, World!");
+var block = new Block(DateTime.Now, new List<Transaction>(), "");
